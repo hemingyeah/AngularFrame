2b09abad17c48fc520fa6e0126f7652050960217
huquanquan <huquanquan@iscs.com.cn>
2016-03-12 18:56:55 +0800

    angular web框架推送

---
+++ Views/Login/Script/Login_Controller.js
@@ -0,0 +1,14 @@
+app
+	.controller('LoginCtrl', function($scope, LoginService, $state, service) {
+		// $scope.app.name = document.title = "网仓3号";
+		$scope.event = {
+			Login: function() {
+				// LoginService.Login($scope, null, function(data) {
+				// 	if (data) {
+						$state.go("GT.Main");
+				// 	}
+				// });
+			}
+		}
+
+	})
