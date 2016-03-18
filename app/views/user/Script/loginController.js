820b9466ccf9550d7c603250266896d8be13cde0
jiangdeqi <36637737@qq.com>
2016-03-18 13:39:16 +0800

    user

---
+++ app/views/user/Script/loginController.js
@@ -0,0 +1,15 @@
+app
+    .controller('LoginCtrl', function($scope, LoginService, $state, service) {
+        $scope.app.settings.headerFixed = false;
+        $scope.app.name = document.title = "网仓3号";
+        $scope.event = {
+            Login: function() {
+                // LoginService.Login($scope, null, function(data) {
+                // 	if (data) {
+                $state.go("iscs.main.tab");
+                // 	}
+                // });
+            }
+        }
+
+    })
