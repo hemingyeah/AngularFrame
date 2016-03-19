3c3dca0d1cee3257133cd0cdf91fa1147f3d939f
jiangdeqi <36637737@qq.com>
2016-03-19 11:41:55 +0800

    密码修改页面

--- app/views/user/Script/passwordRoute.js
+++ app/views/user/Script/passwordRoute.js
@@ -1,40 +1,55 @@
-app
-	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-		$stateProvider
-			.state("passwordcall", {
-				url: '/passwordcall',
-				templateUrl: 'views/user/passwordcall.html',
-				controller: 'passWordCallCtrl',
-				resolve: {
-					deps: ['$ocLazyLoad',
-						function($ocLazyLoad) {
-							return $ocLazyLoad.load([
-								'views/user/script/passwordController.js',
-								'views/user/script/passwordService.js',
-                                'styles/user/drag.css',
-                                'views/user/script/drag.js',
-                                'views/user/script/passWordCall.js'
-							]);
-						}
-					]
-				}
-			})
-            .state('passwordPhone',{
-                url: '/passwordPhone',
-				templateUrl: 'views/user/passwordPhone.html',
-				controller: 'passwordPhoneCtrl',
-				resolve: {
-					deps: ['$ocLazyLoad',
-						function($ocLazyLoad) {
-							return $ocLazyLoad.load([
-								'views/user/script/passwordController.js',
-								// 'views/user/script/passwordService.js',
-                                
-								// 'views/user/script/passWordCall.js'
-								// 'scripts/login.js'
-							]);
-						}
-					]
-				}
-            })
-	}]);
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state("password", {
+            url: '/password',
+            template: '<div ui-view></div>',
+            resolve: {
+                deps: ['$ocLazyLoad',
+                    function($ocLazyLoad) {
+                        return $ocLazyLoad.load([
+                            'views/user/script/passwordController.js',
+                            'views/user/script/passwordService.js',
+                            'views/user/script/passWordCall.js'
+                        ]);
+                    }
+                ]
+            }
+        })
+        //Id页面
+        .state("password.passwordCall", {
+            url: '/passwordCall',
+            template: "<password-call></password-call>",
+            controller: 'passWordCallCtrl',
+            resolve: {
+                deps: ['$ocLazyLoad',
+                    function($ocLazyLoad) {
+                        return $ocLazyLoad.load([
+                            'styles/user/drag.css',
+                            'views/user/script/drag.js',
+                        ]);
+                    }
+                ]
+            }
+        })
+        //Phone页面
+        .state('password.passwordPhone',{
+            url: '/passwordPhone',
+            templateUrl: 'views/user/passwordPhone.html',
+            controller: 'passwordPhoneCtrl'
+            
+        })
+        //Reset页面
+        .state('password.passwordReset',{
+            url: '/passwordReset',
+            template: '<password-reset></password-reset>',
+            controller: 'passwordResetCtrl'
+            
+        })
+        //Phone页面
+        .state('password.passwordSuccess',{
+            url: '/passwordSuccess',
+            templateUrl: 'views/user/passwordSuccess.html',
+            controller: 'passwordSuccessCtrl'
+            
+        })
+}]);
